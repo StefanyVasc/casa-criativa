04cddfab02931f53fa494499ebab3c8e6039100d
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-03-24 23:08:14 -0300

    :sparkles: introducing mordal form
    css ok, html ok, js in development

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnVerIdeias = document.querySelector("button.fat");
-const footerLink = document.querySelector("#footer");
-
-btnVerIdeias.addEventListener("click", function() {
-  footerLink.classList.toggle("hide");
-});
